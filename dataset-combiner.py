81e8cb093a774046b1ee7f0a0e176e95bcec4039
likaiZnazis <Maris.Vitols_2@edu.rtu.lv>
2025-03-25 13:53:32 +0200

    Splits csv file rows into desired amount of fies

--- dataset-combiner.py
+++ dataset-combiner.py
@@ -7,6 +7,8 @@ These segments are placed insdie a single file.
 """""
 import os
 import numpy as np
+import csv
+import math
 #Steps
 '''
 FIRST: need to understand where to place the file. I don't want to change the path each time.
@@ -34,12 +36,46 @@ fileName = ''.join(os.listdir(path))
 pathToCSVFie = os.path.join(path, fileName)
 # print(pathToCSVFie)
 
-
-
 def divide_set():
     try:
-        #absolute path
         mainSet = np.loadtxt(pathToCSVFie, dtype=float, skiprows=1, delimiter=",")
+        #placeing it into 3 separate files
+        setRows = mainSet.shape[0] - (mainSet.shape[0] % 3) #Need to make 3 files out of that single file
+        indexes = [math.ceil(setRows / 3) * line for line in range(4)] #Break row points
+        indexes = indexes[1:-1] #Dont need the first one and last one
+
+        #with ensures that the file is automatically closed when no longer needed
+        with open(pathToCSVFie, newline='') as csvfile:
+            #Reads a single line, default line separator is ","
+            lineReader = csv.reader(csvfile)
+            #For each character in the line that is separated with "," print
+            for index, row in enumerate(lineReader):
+                if(index<1):
+                    #skip the header
+                    continue
+                #for each break number we create a new file
+                for breakIndex in indexes:
+                    if(breakIndex == index):
+                        #close the current file
+                        #create a new file
+                        print(index)
+                        #add the current row
+                # Keep adding rows
+                else:
+                    # print("Ņo")
+                    pass
+                # else:
+                #     print(lineReader.line_num)
+                #     print(index)
+                #     print(", ".join(row))
+                    
+                    # Returns a array - print(row)
+
+        # for line in csvFile - setRows:
+        #     print()
+        #Loop over the whole set c
+        # for 
+        
     except OSError:
         print("OS error occured " + "probably the path is not correct")
     except Exception as err:
@@ -49,7 +85,7 @@ def divide_set():
         print("")
     pass
 
-divide_set()
+# divide_set()
 
 def creat_single_dataset():
     #SECOND step
