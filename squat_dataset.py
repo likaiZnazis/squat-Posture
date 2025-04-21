b3099c731b211825ecc37768612a53965b6117fa
likaiZnazis <Maris.Vitols_2@edu.rtu.lv>
2025-04-21 12:04:17 +0300

    Added set and dataset functions

--- squat_dataset.py
+++ squat_dataset.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass, field
 from squat_set import SquatSet
+import numpy as np
+import traceback
+import preprocessing
+import os
 
 @dataclass
 class Dataset:
@@ -9,4 +13,41 @@ class Dataset:
     #Path to both files
     trainingSetPath: str
     testingSetPath: str
-    
+
+    def resample_segments(self):
+        longest_squat_measured = max(len(segment) for segment in self.segmentArray)
+        segmented_resampled_squats = np.array([np.pad(segment, ((0, longest_squat_measured - len(segment)), (0, 0)), mode='constant') for segment in self.segmentArray])
+        segmented_resampled_squats = np.swapaxes(segmented_resampled_squats, 1, 2)
+        return segmented_resampled_squats
+
+    def combine_sets():
+        try:
+            # Get path to the set files. They will start with a number
+            pathToSetFiles = [ os.path.join(path, file) for file in os.listdir(path) if (file[0].isdigit())]
+            # Get segments from each file
+            all_signals = []
+            for pathSetFile in pathToSetFiles:
+                #load the file
+                currentFile = np.loadtxt(pathSetFile,dtype="float", skiprows=1, delimiter=",")
+                #Get segment indexes
+                segment_indexes = preprocessing.get_segment_indexes(currentFile)
+                #Extract each sensor signal from the segment indexes
+                sensor_signals = preprocessing.extract_each_signal(segment_indexes, currentFile) 
+                #sensor_signals is a 2D array
+                for row in sensor_signals:
+                    all_signals.append(row)
+            return all_signals
+
+        except OSError as err:
+            print("OS error occured " + "probably the path is not correct" + " " + str(err))
+            print(traceback.format_exc())
+        except Exception as err:
+            print("Dont know how to handle this error " + str(err))
+            print(traceback.format_exc())
+            # raise - reraising the error is only useful if there are higher-up try,except
+        except ValueError:
+            print("There was a value error " + str(err))
+            print(traceback.format_exc())
+
+    def split_sets():
+        pass
--- squat_set.py
+++ squat_set.py
@@ -1,5 +1,6 @@
 import numpy as np
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from scipy import stats
 import os
 import preprocessing
 """
@@ -11,10 +12,27 @@ Klases tiks izveidotas, lai būtu vieglāk aplūkot inormāciju.
 class SquatSet:
     fileName: str
     formName: str
-    segmentArray: np.ndarray
+    segmentArray: list[int] = field(default_factory=list)
+    finalArray: list[int] = field(default_factory=list)
     rawData: np.ndarray
     segmentAmountBefore: int = 0
     segmentAmountAfter: int = 0
+
+    SENSOR_MEASURMENTS = {
+        "time": 0,
+        "accX": 1,
+        "accY": 2,
+        "accZ": 3,
+        "gyroX": 4,
+        "gyroY": 5,
+        "gyroZ": 6,
+        "magX": 7,
+        "magY": 8,
+        "magZ": 9,
+        "pitch": 10,
+        "roll": 11,
+        "yaw": 12,
+    }
 
     # Method that will fill out the variables, like segment amount
     def trimSegments(self) -> None:
@@ -23,6 +41,46 @@ class SquatSet:
             self.segmentAmountBefore = len(self.segmentArray)
             self.segmentArray = self.segmentArray[1:len(self.segmentArray)-1]
             self.segmentAmountAfter = len(self.segmentArray)
+    
+    #source - #https://github.com/mlgig/Video_vs_Shimmer_ECML_2023/blob/master/utils/math_funtions.py
+    def get_segment_indexes(self):
+        #Creates a array with starting and ending indexes for each segment
+        segment_signal = self.rawData[:, self.SENSOR_MEASURMENTS["pitch"]]
+        threshold = (np.mean(segment_signal) + np.std(segment_signal)) - 55
+        marker_list = [i >= threshold for i in segment_signal]
+        i = 0
+        final_pairs = []
+        
+        while i < len(segment_signal):
+            if marker_list[i]:  # Start of a segment
+                start = i
+                while i < len(segment_signal) and marker_list[i]:
+                    i += 1
+                end = i - 1  # End of the segment
+                if end - start > 1:  # Ensure segment is significant
+                    final_pairs.append((start, end))
+            i += 1
+        self.segmentArray = final_pairs
+    
+    def extract_each_signal(self):
+        #Using starting and edning indexes extracts each sensor signal from the original dataset. Resulting in individual segments
+        segmented_squats = []
+        for start,end in self.segmentArray:
+            # Iznemam visas vajadzīgās vērtības
+            segment_data = self.rawData[start:end + 1, [self.SENSOR_MEASURMENTS[sensor_signal] for sensor_signal in self.SENSOR_MEASURMENTS]]
+            segmented_squats.append(segment_data)
+        return segmented_squats
+    
+    def getFrequency(self,dataset):
+        #WE load the dataset
+        data = np.loadtxt(dataset,dtype="float", skiprows=1, delimiter=",")
+        #Grab timestamps
+        timestamps = data[:,0]
+        #Get differences between each point
+        #For example 1743403873.08 - 1743403873.10 = 
+        timeStampDifference = np.diff(timestamps)
+        #Get common value
+        return stats.mode(timeStampDifference)
 
 # currentFile = np.loadtxt(os.path.join(os.getcwd(),"dataset","1-Set.csv"),dtype="float", skiprows=1, delimiter=",")
 # segmentedFile = preprocessing.get_segment_indexes(currentFile)
